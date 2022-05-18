78ac52e3a806151110a2124e4b85dfe720497df1
rookie <a1823577945@163.com>
2022-05-18 17:02:31 +0800

    完成版

--- src/main/java/com/rookie/community/controller/HomeController.java
+++ src/main/java/com/rookie/community/controller/HomeController.java
@@ -1,3 +1,7 @@
+
+
+
+
 package com.rookie.community.controller;
 
 import com.rookie.community.pojo.DiscussPost;
--- target/classes/static/js/letter.js
+++ target/classes/static/js/letter.js
@@ -4,26 +4,31 @@ $(function(){
 });
 
 function send_letter() {
-	$("#sendModal").modal("hide");
+
 	var toName = $("#recipient-name").val();
 	var content = $("#message-text").val();
-	$.post(
-		CONTEXT_PATH+"/message/letter/send",
-		{"toName":toName,"content":content},
-		function (data) {
-			data = $.parseJSON(data);
-			if (data.code == 0) {
-				$("#hintBody").text("发送成功！");
-			} else {
-				$("#hintBody").text(data.msg);
+	var b = (JSON.stringify(content).length>2)
+	if (b){
+		$("#sendModal").modal("hide");
+		$.post(
+			CONTEXT_PATH+"/message/letter/send",
+			{"toName":toName,"content":content},
+			function (data) {
+				data = $.parseJSON(data);
+				if (data.code == 0) {
+					$("#hintBody").text("发送成功！");
+				} else {
+					$("#hintBody").text(data.msg);
+				}
+				$("#hintModal").modal("show");
+				setTimeout(function(){
+					$("#hintModal").modal("hide");
+					location.reload();
+				}, 2000);
 			}
-			$("#hintModal").modal("show");
-			setTimeout(function(){
-				$("#hintModal").modal("hide");
-				location.reload();
-			}, 2000);
-		}
-	)
+		)
+	}
+
 
 }
 
